e7d512e7e3f58ef54cd3d8ed5fba1911b71d8950
vidit raj <rockingvidit860@gmail.com>
2020-12-12 19:48:32 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Videly.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
